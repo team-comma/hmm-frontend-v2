31b3ebe4c080363f1d1fb9bbecf538512dbf3299
cmg8431 <cmg8431@gmail.com>
2022-12-02 21:46:42 +0900

    chore(lint): eslint env 설정 추가 및 monorepo 구조 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+  env: {
+    es6: true,
+  },
+};
--- services/web/.eslintrc.js
+++ services/web/.eslintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "react/display-name": "off",
-    "react/jsx-curly-brace-presence": [
-      "error",
-      { props: "never", children: "never" },
-    ],
-  },
-}
+  extends: ['../../.eslintrc.js'],
+};
