30f15a62f16331abab37c06bca4208c3c974a8f6
cmg8431 <cmg8431@gmail.com>
2022-12-10 21:23:56 +0900

    feat(@comma/ds): reset 구조 변경 및 AppLayout 컴포넌트 추가

---
+++ packages/ds/layouts/AppLayout/styled.ts
@@ -0,0 +1,11 @@
+import { styled } from '../../stitches.config';
+
+export const AppLayoutContainer = styled('main', {
+  maxWidth: '35rem',
+  margin: '0px auto',
+  height: '100%',
+});
+
+export const PageTitle = styled('h1', {});
+
+export const PageDescription = styled('p', {});
