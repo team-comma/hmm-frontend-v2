776b2a54bc1728d64275a132634d647bc6f16dab
cmg8431 <cmg8431@gmail.com>
2022-12-13 23:57:44 +0900

    chore(deps): next-image 라이브러리 추가

--- services/web/next.config.js
+++ services/web/next.config.js
@@ -3,6 +3,7 @@
 const withTM = require('next-transpile-modules')(['@comma/ds']);
 
 const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
 
 const nextConfig = {
   reactStrictMode: true,
