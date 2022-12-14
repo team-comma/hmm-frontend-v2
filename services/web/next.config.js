9cb4d76bec421884fe6c29b6b22cdf548698fded
HanSu Lee <i.hansu.lee@gmail.com>
2022-12-15 08:07:32 +0900

    fix(web): `next.config.js` 유효하지 않은 설정 및 플러그인 제거

--- services/web/next.config.js
+++ services/web/next.config.js
@@ -1,18 +1,11 @@
-/* eslint-disable */
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(['@comma/ds']);
-
-const withPlugins = require('next-compose-plugins');
-const withImages = require('next-images');
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['tsx'],
+  experimental: {
+    transpilePackages: ['@comma/ds'],
+  },
 };
 
-module.exports = withPlugins([
-  withTM({
-    nextConfig,
-  }),
-]);
+module.exports = nextConfig;
