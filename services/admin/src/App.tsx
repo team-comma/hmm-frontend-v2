af1ee8656d770b566b654c89d216a161371380a4
HanSu Lee <i.hansu.lee@gmail.com>
2022-12-21 17:42:01 +0900

    feat(@comma/admin): `@comma/ds` global style 적용 및 폰트 추가

--- services/admin/src/App.tsx
+++ services/admin/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import { globalStyles } from '@comma/ds';
+
 import { MainPage } from 'src/pages';
 
 export const App: React.FC = () => {
+  globalStyles();
+
   return (
     <Routes>
       <Route index element={<MainPage />} />
