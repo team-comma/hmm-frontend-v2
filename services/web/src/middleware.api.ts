b83aa602f6733efaa30e2c64ff837df4bc193e57
HanSu Lee <i.hansu.lee@gmail.com>
2023-02-25 16:38:04 +0900

    fix(@comma/web): 변조된 리프레시 토큰의 경우, middleware 무한 리다이렉트 이슈 해결

--- services/web/src/middleware.api.ts
+++ services/web/src/middleware.api.ts
@@ -10,8 +10,7 @@ import { verify } from './utils';
 
 export const middleware: NextMiddleware = async (req: NextRequest) => {
   const { TOKEN_KEY, JWT_SECRET_KEY } = process.env;
-  const { pathname, searchParams, host, protocol } = req.nextUrl;
-
+  const { pathname, host, protocol } = req.nextUrl;
   const generateUrl = (path: string) => `${protocol}//${host}${path}`;
 
   if (!TOKEN_KEY || !JWT_SECRET_KEY)
@@ -22,38 +21,22 @@ export const middleware: NextMiddleware = async (req: NextRequest) => {
   // Next.js static 파일인 경우 허용
   if (pathname.includes('_next')) return;
 
-  if (pathname.startsWith('/login')) {
-    const refreshTokenFromQuery = searchParams.get('refreshToken') ?? '';
-
-    const redirectUrl = refreshTokenFromQuery
-      ? PROTECTED_REDIRECT_URL_FOR_LOGIN
-      : PROTECTED_REDIRECT_URL;
-
-    const url = generateUrl(redirectUrl);
-    const response = NextResponse.redirect(new URL(url));
-
-    if (refreshTokenFromQuery)
-      response.cookies.set(TOKEN_KEY, refreshTokenFromQuery, {
-        httpOnly: true,
-        sameSite: 'strict',
-        path: '/',
-      });
-
-    return response;
-  }
-
-  const refreshToken = req.cookies.get(TOKEN_KEY);
-
+  const refreshToken = req.cookies.get(TOKEN_KEY)?.value ?? '';
+  const deleteCookies: string[] = [];
   let redirectUrl = PROTECTED_REDIRECT_URL;
 
   // 로그인 유저인 경우
   if (refreshToken) {
-    const isValidJWT = await verify(refreshToken.value, JWT_SECRET_KEY);
-    // 올바른 리프레시 토큰 + 허용된 경로 접근 시 -> 허용
-    if (isValidJWT && PROTECTED_PAGES.includes(pathname)) return;
+    const isValidJWT = await verify(refreshToken, JWT_SECRET_KEY);
 
-    // 변조된 리프레시 토큰 또는 허용되지 않은 경로 접근 시 -> 리다이렉트 URL 수정
-    redirectUrl = PROTECTED_REDIRECT_URL_FOR_LOGIN;
+    // 올바른 리프레시 토큰 + 허용된 경로 접근 -> 허용
+    if (isValidJWT && PROTECTED_PAGES.includes(pathname)) return;
+    //올바른 리프레시 토큰 + 허용되지 않은 경로 접근 시 -> /home 리다이렉트
+    if (isValidJWT && !PROTECTED_PAGES.includes(pathname))
+      redirectUrl = PROTECTED_REDIRECT_URL_FOR_LOGIN;
+
+    // 변조된 리프레시 토큰 -> 쿠키 삭제 리스트에 추가
+    if (!isValidJWT) deleteCookies.push(TOKEN_KEY);
   }
 
   // 비로그인 유저인 경우, 로그인 유저만 접근 가능한 경로에 접근하는 경우가 아닌 경우 -> 허용
@@ -61,5 +44,8 @@ export const middleware: NextMiddleware = async (req: NextRequest) => {
 
   // 위 로직에서 핸들링되지 않은 경우 -> 최종 리다이렉트
   const url = generateUrl(redirectUrl);
-  return NextResponse.redirect(new URL(url));
+  const response = NextResponse.redirect(new URL(url), {});
+  deleteCookies.forEach((key) => response.cookies.delete(key));
+
+  return response;
 };
